f92d507c680dc35239aeeb36208c2af88ff82f11
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
1998-07-11 16:10:24 +0000

    Import of ANDK/Date-GetDate-2.01 from CPAN.
    gitpan-cpan-distribution: Date-GetDate
    gitpan-cpan-version:      2.01
    gitpan-cpan-path:         ANDK/Date-GetDate-2.01.tar.gz
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     released

--- GetDate.pm
+++ GetDate.pm
@@ -5,9 +5,18 @@ require DynaLoader;
 use vars qw(@ISA @EXPORT $VERSION);
 @ISA = qw(Exporter DynaLoader);
 @EXPORT = qw( getdate );
-$VERSION = "2.00";
+$VERSION = "2.01";
+warn "Date::GetDate needs Date::Parse for dates in the third millennium";
 
-sub getdate { &salz_getdate }
+sub getdate {
+  my($ret);
+  if (($ret = &salz_getdate) < 0) {
+    require Date::Parse;
+    return &Date::Parse::str2time;
+  } else {
+    return $ret;
+  }
+}
 
 bootstrap Date::GetDate;
 1;
